31cf4c32ab0de6c3be97173b0db403b618f8f96f
shcherby <anton.shcherbyna@hpe.com>
2025-05-15 10:51:27 +0200

    Clean up

--- benefits-calc/Api/Application/Employees/Queries/EmployeeQuery.cs
+++ benefits-calc/Api/Application/Employees/Queries/EmployeeQuery.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Api.Application.Dependents.Payload;
 using Api.Application.Employees.Payload;
 using Api.Data;
@@ -21,57 +22,43 @@ public class EmployeeQuery : IEmployeeQuery
     // it helps to reduce the amount of data sent over the network and reduce performance issues
     public async Task<IReadOnlyCollection<GetEmployeeDto>> GetAll(CancellationToken cancellationToken)
     {
-        // why? Use of a projection to select only the necessary fields from the database.
         var employees = await _context
             .Employees
             .AsNoTracking()
-            .Select(e => new GetEmployeeDto
-            {
-                Id = e.Id,
-                FirstName = e.FirstName,
-                LastName = e.LastName,
-                Salary = e.Salary,
-                DateOfBirth = e.DateOfBirth,
-                Dependents = e.Dependents.Select(d => new GetDependentDto
-                {
-                    Id = d.Id,
-                    FirstName = d.FirstName,
-                    LastName = d.LastName,
-                    Relationship = d.Relationship,
-                    DateOfBirth = d.DateOfBirth
-                }).ToList()
-            }).ToListAsync(cancellationToken);
+            .Select(GetEmployeeProjection)
+            .ToListAsync(cancellationToken);
         return employees;
     }
 
+    // why? The generated SQL includes a subquery in the JOIN clause, which may be suboptimal for performance.
+    // Consider using raw SQL, rewriting the LINQ expression, or applying AsSplitQuery().
+    // However, review the actual execution plan and SQL performance before making changes.
     public async Task<GetEmployeeDto> GetById(int id, CancellationToken cancellationToken)
     {
-        Employee? emp = await _context.Employees
+        var emp = await _context.Employees
             .Include(e => e.Dependents)
             .AsNoTracking()
-            .FirstOrDefaultAsync(d => d.Id == id, cancellationToken);
+            .Where(d => d.Id == id)
+            .Select(GetEmployeeProjection)
+            .FirstOrDefaultAsync(cancellationToken);
         if (emp is null) throw new ApplicationException("Employee not found");
-        return Map(emp);
+        return emp;
     }
 
-    // Why? Use auto mapper which simplifies object-to-object mapping and reduces boilerplate code.
-    private static GetEmployeeDto Map(Employee emp)
+    private static readonly Expression<Func<Employee, GetEmployeeDto>> GetEmployeeProjection = e => new GetEmployeeDto
     {
-        return new GetEmployeeDto
+        Id = e.Id,
+        FirstName = e.FirstName,
+        LastName = e.LastName,
+        Salary = e.Salary,
+        DateOfBirth = e.DateOfBirth,
+        Dependents = e.Dependents.Select(d => new GetDependentDto
         {
-            Id = emp.Id,
-            FirstName = emp.FirstName,
-            LastName = emp.LastName,
-            Salary = emp.Salary,
-            DateOfBirth = emp.DateOfBirth,
-            Dependents = emp.Dependents.Select(d => new GetDependentDto
-            {
-                Id = d.Id,
-                FirstName = d.FirstName,
-                LastName = d.LastName,
-                Relationship = d.Relationship,
-                DateOfBirth = d.DateOfBirth
-            }).ToList()
-        };
-    }
+            Id = d.Id,
+            FirstName = d.FirstName,
+            LastName = d.LastName,
+            Relationship = d.Relationship,
+            DateOfBirth = d.DateOfBirth
+        }).ToList()
+    };
 }
